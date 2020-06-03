c5bc66baf729d10b771c2be8d2f51a508f616624
Nanar Boursalian <nanar.boursalian01@gmail.com>
2020-06-03 12:06:28 -0700

    Included Instagram, Discord, and LinkedIn icons

--- src/Components/Navbar/UserNavbar.js
+++ src/Components/Navbar/UserNavbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Collapse,
   NavbarToggler,
@@ -11,24 +11,77 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem
-} from 'reactstrap';
-import { membershipState } from '../../Enums';
-import logo from '../Navbar/sce_logo.png';
+} from "reactstrap";
+import { membershipState } from "../../Enums";
+import logo from "../Navbar/sce_logo.png";
 
 export default function UserNavBar(props) {
   const [collapsed, setCollapsed] = useState(true);
-  const unauthedRoutes = [{ title: 'Events', route: '/events' }];
+  const icons = [
+    {
+      link: ["https://www.linkedin.com/company", "/sjsusce/"].join(""),
+      vector: [
+        "M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762",
+        " 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.",
+        "5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79",
+        " 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.",
+        "113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+      ].join("")
+    },
+    {
+      link: "https://discord.com/invite/STkT6mH",
+      vector: [
+        "M19.54 0c1.356 0 2.46 1.104 2.46 2.472v21.528l-2.58-2.28-1.452-1.",
+        "344-1.536-1.428.636 2.22h-13.608c-1.356 0-2.46-1.104-2.46-2.472v-16.",
+        "224c0-1.368 1.104-2.472 2.46-2.472h16.08zm-4.632 15.672c2.652-.084 3.",
+        "672-1.824 3.672-1.824 0-3.864-1.728-6.996-1.728-6.996-1.728-1.296-3.",
+        "372-1.26-3.372-1.26l-.168.192c2.04.624 2.988 1.524 2.988 1.524-1.248-.",
+        "684-2.472-1.02-3.612-1.152-.864-.096-1.692-.072-2.424.024l-.204.024c-.",
+        "42.036-1.44.192-2.724.756-.444.204-.708.348-.708.348s.996-.948 3.156-1.",
+        "572l-.12-.144s-1.644-.036-3.372 1.26c0 0-1.728 3.132-1.728 6.996 0 0 1.",
+        "008 1.74 3.66 1.824 0 0 .444-.54.804-.996-1.524-.456-2.1-1.416-2.1-1.",
+        "416l.336.204.048.036.047.027.014.006.047.027c.3.168.6.3.876.408.492.192",
+        " 1.08.384 1.764.516.9.168 1.956.228 3.108.012.564-.096 1.14-.264 1.74-.516.",
+        "42-.156.888-.384 1.38-.708 0 0-.6.984-2.172 1.428.36.456.792.972.792.",
+        "972zm-5.58-5.604c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684",
+        " 0 1.224-.6 1.224-1.332.012-.732-.54-1.332-1.224-1.332zm4.38 0c-.684 0-1.",
+        "224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.",
+        "332 0-.732-.54-1.332-1.224-1.332z"
+      ].join("")
+    },
+    {
+      link: "https://www.instagram.com/sjsusce/",
+      vector: [
+        "M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058",
+        " 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664",
+        " 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.",
+        "07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.",
+        "204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.",
+        "069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.",
+        "98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358",
+        " 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.",
+        "072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.",
+        "014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.",
+        "949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.",
+        "162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.",
+        "79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.",
+        "441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.",
+        "44-1.439-1.44z"
+      ].join("")
+    }
+  ];
+  const unauthedRoutes = [{ title: "Events", route: "/events" }];
 
   return (
-    <div className='user-nav'>
-      <Navbar light expand='md'>
-        <NavbarBrand href='/'>
+    <div className="user-nav">
+      <Navbar light expand="md">
+        <NavbarBrand href="/">
           <div>
             <img
               src={logo}
-              alt={'sce logo'}
+              alt={"sce logo"}
               style={{
-                width:50,
+                width: 50,
                 marginTop: -7,
                 marginBottom: -7,
                 marginLeft: 10
@@ -38,20 +91,31 @@ export default function UserNavBar(props) {
         </NavbarBrand>
         <NavbarToggler
           onClick={() => setCollapsed(!collapsed)}
-          className='mr-2'
+          className="mr-2"
         />
         <Collapse isOpen={!collapsed} navbar>
-          <Nav className='ml-auto sce-nav' navbar>
+          <Nav className="ml-auto sce-nav" navbar>
+            {icons.map((icon, index) => {
+              return (
+                <a key={index} href={icon.link}>
+                  <svg className="m-2 icon-images" viewBox="0 0 24 24">
+                    <path fill="white" d={icon.vector} />
+                  </svg>
+                </a>
+              );
+            })}
             {props.user && props.user.accessLevel >= membershipState.MEMBER && (
               <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret
-                  style={{color: 'white', fontWeight: 'bold'}}
+                <DropdownToggle
+                  nav
+                  caret
+                  style={{ color: "white", fontWeight: "bold" }}
                 >
                   Printing
                 </DropdownToggle>
                 <DropdownMenu right>
-                  <DropdownItem href='/2DPrinting'>2D Printing</DropdownItem>
-                  <DropdownItem href='/3DPrintingForm'>
+                  <DropdownItem href="/2DPrinting">2D Printing</DropdownItem>
+                  <DropdownItem href="/3DPrintingForm">
                     3D Printing
                   </DropdownItem>
                 </DropdownMenu>
@@ -61,7 +125,7 @@ export default function UserNavBar(props) {
               return (
                 <NavItem key={index}>
                   <NavLink
-                    style={{color: 'white', fontWeight: 'bold'}}
+                    style={{ color: "white", fontWeight: "bold" }}
                     href={link.route}
                   >
                     {link.title}
@@ -71,36 +135,38 @@ export default function UserNavBar(props) {
             })}
             {props.authenticated && props.user ? (
               <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret style={{color: 'white'}}>
+                <DropdownToggle nav caret style={{ color: "white" }}>
                   <svg
-                    style={{ width: '30px', height: '30px' }}
-                    viewBox='0 0 24 24'
+                    style={{ width: "30px", height: "30px" }}
+                    viewBox="0 0 24 24"
                   >
                     <path
-                      fill='#AAAAAA'
-                      d='M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,
+                      fill="#AAAAAA"
+                      d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,
                       12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,
                       19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,
                       8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,
-                      22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z'
+                      22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"
                     />
                   </svg>
                   {props.user.firstName}
                 </DropdownToggle>
                 <DropdownMenu right>
-                  <DropdownItem href='/profile'>Profile</DropdownItem>
+                  <DropdownItem href="/profile">Profile</DropdownItem>
                   {props.user.accessLevel >= membershipState.OFFICER && (
-                    <DropdownItem href='/dashboard'>Admin</DropdownItem>
+                    <DropdownItem href="/dashboard">Admin</DropdownItem>
                   )}
                   <DropdownItem>
                     <div onClick={() => props.handleLogout()}>
                       <svg
-                        style={{ width: '18px', height: '18px' }}
-                        viewBox='0 0 24 24'
+                        style={{ width: "18px", height: "18px" }}
+                        viewBox="0 0 24 24"
                       >
-                        <path d='M17,17.25V14H10V10H17V6.75L22.25,12L17,
+                        <path
+                          d="M17,17.25V14H10V10H17V6.75L22.25,12L17,
                         17.25M13,2A2,2 0 0,1 15,4V8H13V4H4V20H13V16H15V20A2,
-                        2 0 0,1 13,22H4A2,2 0 0,1 2,20V4A2,2 0 0,1 4,2H13Z' />
+                        2 0 0,1 13,22H4A2,2 0 0,1 2,20V4A2,2 0 0,1 4,2H13Z"
+                        />
                       </svg>
                       Logout
                     </div>
@@ -109,18 +175,22 @@ export default function UserNavBar(props) {
               </UncontrolledDropdown>
             ) : (
               <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret
-                  style={{color: 'white', marginRight: 10, fontWeight: 'bold'}}
+                <DropdownToggle
+                  nav
+                  caret
+                  style={{
+                    color: "white",
+                    marginRight: 10,
+                    fontWeight: "bold"
+                  }}
                 >
                   Join Us!
                 </DropdownToggle>
                 <DropdownMenu right>
-                  <DropdownItem href='/register'>
+                  <DropdownItem href="/register">
                     Membership Application
                   </DropdownItem>
-                  <DropdownItem href='/login'>
-                    Login
-                  </DropdownItem>
+                  <DropdownItem href="/login">Login</DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
             )}
